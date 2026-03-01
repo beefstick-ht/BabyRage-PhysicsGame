627c4365cd7f8da40281ff34cc8912b7cd0ef239
sleepynills <skrasniq@pratt.edu>
2026-03-01 14:48:19 -0500

    Buttons work

--- Assets/Scripts/Play Button.cs
+++ Assets/Scripts/Play Button.cs
@@ -1,16 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayButton : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void Play()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("GameTest");
     }
 }
