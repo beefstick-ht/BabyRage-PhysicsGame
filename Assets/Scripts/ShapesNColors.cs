e68606aa53af310efea435051569b9a9c5bfc867
beefstick-ht <ctrickel@pratt.edu>
2026-03-01 14:37:29 -0500

    Triggerstart

--- Assets/Scripts/ShapesNColors.cs
+++ Assets/Scripts/ShapesNColors.cs
@@ -2,68 +2,22 @@ using UnityEngine;
 
 public class ShapesNColors : MonoBehaviour
 {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-=======
 
-
-
->>>>>>> Stashed changes
-=======
-
-
-
->>>>>>> Stashed changes
-=======
-
-
-
->>>>>>> Stashed changes
     void Start()
     {
         
     }
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    // Update is called once per frame
-=======
-  
->>>>>>> Stashed changes
-=======
-  
->>>>>>> Stashed changes
-=======
-  
->>>>>>> Stashed changes
     void Update()
     {
         
     }
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "RedSquare")
         {
-            GetComponent<Renderer>().color = Color.white;
+            Debug.Log("fard");
         }
     }
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 }
