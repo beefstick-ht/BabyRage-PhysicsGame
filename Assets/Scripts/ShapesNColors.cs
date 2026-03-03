2ccd30dad54affa86aa738026084a10b6b2c036d
beefstick-ht <ctrickel@pratt.edu>
2026-03-03 02:40:34 -0500

    ButtonTags

--- Assets/Scripts/ShapesNColors.cs
+++ Assets/Scripts/ShapesNColors.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class ShapesNColors : MonoBehaviour
 {
-    public GameObject cube; 
+    public GameObject cube;
+    public bool redActive;
+    public bool blueActive;
+    public bool greenActive;
 
     void Start()
     {
-        
+        redActive = false;
+        blueActive = false;
+        greenActive = false;
     }
 
     void Update()
@@ -19,7 +24,30 @@ public class ShapesNColors : MonoBehaviour
         if (other.tag == "RedCube")
         {
             Debug.Log("fard");
-
+            redActive = true;
         }
+        if (other.tag == "BlueCube")
+        {
+            Debug.Log("gwopt");
+            blueActive = true;
+        }
+        if (other.tag == "GreenCube")
+        {
+            Debug.Log("blunk");
+            greenActive = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+
+        Debug.Log("aw");
+        redActive = false;
+
+        Debug.Log("darn");
+        blueActive = false;
+
+        Debug.Log("sghoort");
+        greenActive = false;
+        
     }
 }
