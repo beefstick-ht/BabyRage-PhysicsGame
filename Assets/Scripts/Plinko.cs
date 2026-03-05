f37c5614cc00bededca42ea350011520be81fac3
beefstick-ht <ctrickel@pratt.edu>
2026-03-05 14:35:59 -0500

    Plink4

--- Assets/Scripts/Plinko.cs
+++ Assets/Scripts/Plinko.cs
@@ -2,19 +2,80 @@ using UnityEngine;
 
 public class Plinko : MonoBehaviour
 {
+    [Header("Cubes")]
+    public GameObject redCube;
+    public GameObject blueCube;
+    public GameObject greenCube;
 
+    public bool redActive;
+    public bool blueActive;
+    public bool greenActive;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject redButton;
+    public GameObject greenButton;
+    public GameObject blueButton;
+
+    public Material redEmission;
+    public Material redNormal;
+    public Material blueEmission;
+    public Material blueNormal;
+    public Material greenEmission;
+    public Material greenNormal;
+
+    public MeshRenderer redRenderer;
+    public MeshRenderer blueRenderer;
+    public MeshRenderer greenRenderer;
+
+    public GameObject door;
+
     void Start()
     {
+        redActive = false;
+        blueActive = false;
+        greenActive = false;
 
+        redRenderer = redButton.GetComponent<MeshRenderer>();
+        blueRenderer = blueButton.GetComponent<MeshRenderer>();
+        greenRenderer = greenButton.GetComponent<MeshRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (redActive & blueActive & greenActive == true)
+        {
+            door.SetActive(false);
+        }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "RedCube")
+        {
+            Debug.Log("fard");
+            redActive = true;
+            redRenderer.material = redEmission;
+        }
+        if (other.tag == "BlueCube")
+        {
+            Debug.Log("gwopt");
+            blueActive = true;
+            blueRenderer.material = blueEmission;
 
+        }
+        if (other.tag == "GreenCube")
+        {
+            Debug.Log("blunk");
+            greenActive = true;
+            greenRenderer.material = greenEmission;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "GreenCube")
+        {
+            Debug.Log("blunk");
+            greenActive = false;
+            greenRenderer.material = greenNormal;
+        }
+    }
 }
